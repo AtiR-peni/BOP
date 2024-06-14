5c72b2351758dbe9fb0b1d3bfa8018a0663a6426
AtiR-peni <ryota.k128@gmail.com>
2024-06-14 11:42:42 +0900

    Create start.dart

---
+++ flutter_kadai/lib/start.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class StartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/background.jpg',
+            fit: BoxFit.cover,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '収支表アプリ',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 50,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black,
+                      offset: Offset(5.0, 5.0),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => IncomeExpenseHomePage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                child: Text('開始'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
